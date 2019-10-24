3c087bbab979bb0c1d6e7bd47e297713c781f9db
alebogado <40049133+aleisma@users.noreply.github.com>
2019-10-24 16:09:48 -0300

    Update SalvoController.java

--- src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -73,27 +73,5 @@ public class SalvoController {
 
         }
 
-    @RequestMapping(path = "/players", method = RequestMethod.POST)
-    public ResponseEntity<Object> register(
-            @RequestParam String userName, @RequestParam String password) {
-
-        if (userName.isEmpty() || password.isEmpty()) {
-            return new ResponseEntity<>("Missing data", HttpStatus.FORBIDDEN);
-        }
-
-        if (playerRepository.findByUserName(userName) !=  null) {
-            return new ResponseEntity<>("Name already in use", HttpStatus.FORBIDDEN);
-        }
-
-        playerRepository.save(new Player( userName, passwordEncoder.encode(password)));
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-
-
-
-
-
-
-
 
 }
