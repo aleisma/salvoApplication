aa4404fda28f8c3414753763834e0e5e3e176a95
aleisma <aleismabogado@gmail.com>
2019-11-06 14:47:35 -0300

    task 11, tie

--- src/main/java/com/codeoftheweb/salvo/controller/GameController.java
+++ src/main/java/com/codeoftheweb/salvo/controller/GameController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -33,35 +34,35 @@ public class GameController {
 
         GamePlayer gamePlayer = gamePlayerRepository.findById(gp).orElse(null);
 
-        if(gamePlayer == null ){
-            return new  ResponseEntity<>(makeMap("error","EL GAMEPLAYER NO EXISTE"),HttpStatus.UNAUTHORIZED);
+        if (gamePlayer == null) {
+            return new ResponseEntity<>(makeMap("error", "EL GAMEPLAYER NO EXISTE"), HttpStatus.UNAUTHORIZED);
         }
 
-        if(isGuest(authentication)){
-            return new  ResponseEntity<>(makeMap("error","NO PUEDE ACCEDER A ESTA VISTA"),HttpStatus.UNAUTHORIZED);
+        if (isGuest(authentication)) {
+            return new ResponseEntity<>(makeMap("error", "NO PUEDE ACCEDER A ESTA VISTA"), HttpStatus.UNAUTHORIZED);
         }
-        Player  player  = playerRepository.findByUserName(authentication.getName());
+        Player player = playerRepository.findByUserName(authentication.getName());
 
         Game game = gamePlayer.getGames();
 
-        if(player == null){
-            return new  ResponseEntity<>(makeMap("error","PLAYER NO EXISTE"),HttpStatus.UNAUTHORIZED);
+        if (player == null) {
+            return new ResponseEntity<>(makeMap("error", "PLAYER NO EXISTE"), HttpStatus.UNAUTHORIZED);
         }
 
-        if(gamePlayer == null ){
-            return  new ResponseEntity<>(makeMap("error"," GP NO EXISTE"), HttpStatus.UNAUTHORIZED);
+        if (gamePlayer == null) {
+            return new ResponseEntity<>(makeMap("error", " GP NO EXISTE"), HttpStatus.UNAUTHORIZED);
         }
 
-        if(gamePlayer.getPlayer().getId() !=  player.getId()){
-            return new  ResponseEntity<>(makeMap("error","NO PUEDE ACCEDER A ESTA VISTA"),HttpStatus.CONFLICT);
+        if (gamePlayer.getPlayer().getId() != player.getId()) {
+            return new ResponseEntity<>(makeMap("error", "NO PUEDE ACCEDER A ESTA VISTA"), HttpStatus.CONFLICT);
         }
 
-      if (gamePlayer.getPlayer() == player) {
+        if (gamePlayer.getPlayer() == player) {
 
-          return new ResponseEntity<>(gameViewDTO(gamePlayer), HttpStatus.OK);
+            return new ResponseEntity<>(gameViewDTO(gamePlayer), HttpStatus.OK);
         }
 
-        return new  ResponseEntity<>(makeMap("ERROR!!","NO PUEDE ACCEDER A ESTA VISTA"),HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(makeMap("ERROR!!", "NO PUEDE ACCEDER A ESTA VISTA"), HttpStatus.UNAUTHORIZED);
     }
 
     //====================== DTO GAMEVIEW =============================================================================/
@@ -89,9 +90,9 @@ public class GameController {
                     gamePlayer2 = gamePlayer;
                 }
             }
-                mapa.put("self", selfopponentDTO(gamePlayer1, gamePlayer2));
-                mapa.put("opponent", selfopponentDTO(gamePlayer2, gamePlayer1));
-                return mapa;
+            mapa.put("self", selfopponentDTO(gamePlayer1, gamePlayer2));
+            mapa.put("opponent", selfopponentDTO(gamePlayer2, gamePlayer1));
+            return mapa;
         }
         mapa.put("self", selfopponentDTO(gamePlayer1, new GamePlayer()));
         mapa.put("opponent", selfopponentDTO(new GamePlayer(), gamePlayer1));
@@ -104,12 +105,9 @@ public class GameController {
 
         Set<Salvo> gp2salvos = gamePlayer2.getSalvoes();
 
-            if( gamePlayer2 == null)
-            {
-                List<String> lista = new ArrayList<>();
-            }
-
-      else  {
+        if (gamePlayer2 == null) {
+            List<String> lista = new ArrayList<>();
+        } else {
             // Ordeno para que se muestren bien los turnos
             List<Salvo> OrderedSalvoes = gp2salvos.stream().sorted(Comparator
                     .comparing(salvo -> salvo.getTurn()))
@@ -206,19 +204,19 @@ public class GameController {
     //====================== MAKE SALVOS ==============================================================================/
     private List<Map> makeSalvos(Set<GamePlayer> gp) {
 
-        Stream<Salvo> ss = gp.stream().flatMap(g -> g.getSalvoes() .stream());
+        Stream<Salvo> ss = gp.stream().flatMap(g -> g.getSalvoes().stream());
 
         return ss.map(salvo -> salvo.makeSalvoDTO()).collect((Collectors.toList()));
     }
 
     //======================== GET OPPONENT ===========================================================================/
-    private Optional <GamePlayer> getOpponent(GamePlayer self){
+    private Optional<GamePlayer> getOpponent(GamePlayer self) {
 
         return self.getGame().getGamePlayers().stream().filter(gamePlayer -> gamePlayer.getId() != self.getId()).findFirst();
     }
 
     //=================== CHECKING GAME STATE =========================================================================/
-    private GameState checkGameState (GamePlayer gamePlayer1) {
+    private GameState checkGameState(GamePlayer gamePlayer1) {
 
         int sumatoria;
         int sumatoria2;
@@ -228,7 +226,7 @@ public class GameController {
             return GameState.PLACESHIPS;
         }
 
-        if (gamePlayer1.getGame().getGamePlayers().size() == 1){
+        if (gamePlayer1.getGame().getGamePlayers().size() == 1) {
             return GameState.WAITINGFOROPP;
         }
 
@@ -236,87 +234,75 @@ public class GameController {
 
             GamePlayer opponent = getOpponent(gamePlayer1).orElse(null);
 
-            for (GamePlayer gamePlayer : gamePlayer1.getGame().getGamePlayers()) {
 
-                // Consigo sumatoria de hits de ambos players
-                sumatoria = sumatoriaHits(gamePlayer1,opponent);
-                sumatoria2 = sumatoriaHits(opponent, gamePlayer1);
+            // Consigo sumatoria de hits de ambos players
+            sumatoria = hits(gamePlayer1);
 
-                if(opponent  != null){
-                    if ( (gamePlayer1.getSalvoes().size() == opponent.getSalvoes().size()) && (gamePlayer1.getId() < opponent.getId())
-                            && (sumatoria !=17) && (sumatoria2 !=17)) {
-                        return GameState.PLAY;
-                    }
-                }
+            sumatoria2 = hits(opponent);
 
-                if (gamePlayer1.getSalvoes().size() < opponent.getSalvoes().size() && (sumatoria !=17) && (sumatoria2 !=17)){
-                    return GameState.PLAY;
-                }
-                if ((gamePlayer1.getSalvoes().size() == opponent.getSalvoes().size()) && (gamePlayer1.getId() > opponent.getId())  ) {
-                    return GameState.WAIT;
-                }
-                if ((gamePlayer1.getSalvoes().size() > opponent.getSalvoes().size()) && (sumatoria !=17 && sumatoria2 !=17 )){
-                    return GameState.WAIT;
-                }
-
-
-                System.out.println("hits player 1: "+ sumatoria);
-                System.out.println("hits player 2: "+ sumatoria2);
-
-                if ( (gamePlayer1.getSalvoes().size() == opponent.getSalvoes().size()) && (sumatoria==17 && sumatoria2==17)) {
-
-
-                    System.out.println("hits player 1 en if: "+ sumatoria);
-                    System.out.println("hits player 2 en if: "+ sumatoria2);
-
-                    return GameState.TIE;
-
-                }
-
-              if ((gamePlayer1.getSalvoes().size() == opponent.getSalvoes().size())  && (sumatoria==17)  ) {
-                    return GameState.WON;
-                }
-
-                if ((gamePlayer1.getSalvoes().size() == opponent.getSalvoes().size()) ) {
-                    return GameState.LOST;
-                }
+            if ((gamePlayer1.getSalvoes().size() == opponent.getSalvoes().size()) && (gamePlayer1.getId() < opponent.getId())
+                    && (sumatoria != 17) && (sumatoria2 != 17)) {
+                return GameState.PLAY;
             }
 
+            if (gamePlayer1.getSalvoes().size() < opponent.getSalvoes().size() && (sumatoria != 17) && (sumatoria2 != 17)) {
+                return GameState.PLAY;
+            }
+            if ((gamePlayer1.getSalvoes().size() == opponent.getSalvoes().size()) && (gamePlayer1.getId() > opponent.getId())) {
+                return GameState.WAIT;
+            }
+            if ((gamePlayer1.getSalvoes().size() > opponent.getSalvoes().size()) && (sumatoria != 17 && sumatoria2 != 17)) {
+                return GameState.WAIT;
+            }
+
+
+            System.out.println("hits player 1: " + sumatoria);
+            System.out.println("hits player 2: " + sumatoria2);
+
+
+
+            if ((gamePlayer1.getSalvoes().size() == opponent.getSalvoes().size()) && (sumatoria == 17 && sumatoria2 == 17)) {
+
+                System.out.println("hits player 1 en if: " + sumatoria);
+                System.out.println("hits player 2 en if: " + sumatoria2);
+
+                return GameState.TIE;
+            }
+
+            if ((gamePlayer1.getSalvoes().size() == opponent.getSalvoes().size()) && (sumatoria == 17)) {
+                return GameState.WON;
+            }
+
+            if ((gamePlayer1.getSalvoes().size() == opponent.getSalvoes().size())) {
+                return GameState.LOST;
+            }
         }
+
         return GameState.UNDEFINED;
     }
 
     //============================ SUMATORIA DE HITS ==================================================================/
-    private int sumatoriaHits(GamePlayer gamePlayer1 , GamePlayer opponent) {
+    private int hits(GamePlayer gamePlayer1) {
+
         int sumatoria = 0;
-        Set<Salvo> gp2salvos = opponent.getSalvoes();
 
+        GamePlayer opponent = getOpponent(gamePlayer1).orElse(null);
 
+        List<Salvo> opponent_salvoes = opponent.getSalvoes()
+                .stream()
+                .sorted((a, b) -> a.getTurn() - b.getTurn())
+                .collect(Collectors.toList());
 
-
-
-        for (Salvo salvo : gp2salvos) { // Cada salvo
-            for (String salvoLocation : salvo.getLocations()) { // Cada salvo location
-                boolean hit = false;
-                for (Ship ship : gamePlayer1.getShips()) {  // Cada ship
-
-                    for (String shipLocation : ship.getLocations()) {// Cada ship location
-
-                        if (salvoLocation == shipLocation) {
-                            hit = true;
-                            sumatoria++;
-                        }
-                        else {
-                            hit = false;
-                        }
-                        if (hit) break;
+        for (Salvo salvo : opponent_salvoes) { // Cada salvo
+            for (Ship ship : gamePlayer1.getShips()) {  // Cada ship
+                for (String shipLocation : ship.getLocations()) {//// Cada ship location
+                    if (salvo.getLocations().contains(shipLocation)) {
+                        sumatoria++;
                     }
-                    if (hit) break;
                 }
             }
         }
         return sumatoria;
-
     }
 
     //====================== ALL GAMES ================================================================================/
@@ -324,17 +310,16 @@ public class GameController {
     public Map<String, Object> getGameAll(Authentication authentication) {
 
         Map<String, Object> dto = new LinkedHashMap<String, Object>();
-        if(isGuest(authentication)){
+        if (isGuest(authentication)) {
             dto.put("player", "Guest");
-        }
-        else{
-            Player player  = playerRepository.findByUserName(authentication.getName());
+        } else {
+            Player player = playerRepository.findByUserName(authentication.getName());
             dto.put("player", player.makePlayerDTO());
         }
 
         dto.put("games", gameRepository.findAll()
                 .stream()
-                .map(games->games.makeGameDTO()));
+                .map(games -> games.makeGameDTO()));
 
         return dto;
     }
@@ -347,22 +332,22 @@ public class GameController {
             return new ResponseEntity<>("NO esta autorizado", HttpStatus.UNAUTHORIZED);
         }
 
-        Player player  = playerRepository.findByUserName(authentication.getName());
+        Player player = playerRepository.findByUserName(authentication.getName());
 
-        if(player ==  null){
+        if (player == null) {
             return new ResponseEntity<>("NO esta autorizado", HttpStatus.UNAUTHORIZED);
         }
 
-        Game game  = gameRepository.save(new Game());
+        Game game = gameRepository.save(new Game());
 
-        GamePlayer gamePlayer  = gamePlayerRepository.save(new GamePlayer(player,game));
+        GamePlayer gamePlayer = gamePlayerRepository.save(new GamePlayer(player, game));
 
-        return new ResponseEntity<>(makeMap("gpid",gamePlayer.getId()),HttpStatus.CREATED);
+        return new ResponseEntity<>(makeMap("gpid", gamePlayer.getId()), HttpStatus.CREATED);
     }
 
     private Map<String, Object> makeMap(String key, Object value) {
         Map<String, Object> map = new HashMap<>();
-        map.put(key,value);
+        map.put(key, value);
         return map;
     }
 
