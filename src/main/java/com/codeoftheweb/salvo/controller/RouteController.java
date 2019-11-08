b6931d2aced2940232547e654c0f08be93954772
alebogado <40049133+aleisma@users.noreply.github.com>
2019-11-08 17:28:02 -0300

    Update RouteController.java

--- src/main/java/com/codeoftheweb/salvo/controller/RouteController.java
+++ src/main/java/com/codeoftheweb/salvo/controller/RouteController.java
@@ -2,8 +2,8 @@ package com.codeoftheweb.salvo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
+@RequestMapping("/")
 public class RouteController {
     @RequestMapping(value = "/")
     public String redictectPage(){
