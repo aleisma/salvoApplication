9bcee41c558b57a5a3cecee3a43dc8a8313a39c5
aleisma <aleismabogado@gmail.com>
2019-11-08 16:53:29 -0300

    agregando ruta home

--- src/main/java/com/codeoftheweb/salvo/repositories/ScoreRepository.java
+++ src/main/java/com/codeoftheweb/salvo/repositories/ScoreRepository.java
@@ -1,5 +1,7 @@
 package com.codeoftheweb.salvo.repositories;
 
+import com.codeoftheweb.salvo.model.GamePlayer;
+import com.codeoftheweb.salvo.model.Player;
 import com.codeoftheweb.salvo.model.Score;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -8,4 +10,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface ScoreRepository extends JpaRepository<Score, Long> {
 
+    Player findScorePlayer( GamePlayer gamePlayer, Player player);
 }
