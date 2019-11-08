add3389c284d96e0b62ac6a88b9242f799bf5f9e
alebogado <40049133+aleisma@users.noreply.github.com>
2019-11-08 17:08:11 -0300

    Update RouteController.java

--- src/main/java/com/codeoftheweb/salvo/controller/RouteController.java
+++ src/main/java/com/codeoftheweb/salvo/controller/RouteController.java
@@ -1,10 +1,18 @@
 package com.codeoftheweb.salvo.controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/")
+
+@Controller
 public class RouteController {
     @RequestMapping(value = "/")
     public String redictectPage(){
         return "redirect:/web/games.html";
     }
 }
+
+
+
+
+
+
