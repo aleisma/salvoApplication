8d74e92609068982ca8f62c99848023d627b9585
aleisma <aleismabogado@gmail.com>
2019-11-14 10:08:45 -0300

    implementando mysql, para que persista la info

--- src/main/java/com/codeoftheweb/salvo/controller/RouteController.java
+++ src/main/java/com/codeoftheweb/salvo/controller/RouteController.java
@@ -1,7 +1,9 @@
 package com.codeoftheweb.salvo.controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/")
+
+@Controller
 public class RouteController {
     @RequestMapping(value = "/")
     public String redictectPage(){
